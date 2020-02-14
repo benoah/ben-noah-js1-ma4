9d83aff8bb8cf4f26cf098107889e80a68b45bc3
Ben Noah <noahbenaas@gmail.com>
2020-02-14 20:15:19 +0100

    Update question3.js

--- question3/question3.js
+++ question3/question3.js
@@ -14,16 +14,91 @@ fetch(baseUrl)
 
 
 
-function createGameDetails(json){
+function createGameDetails(json) {
+    console.dir(json);
 
-    const list = document.querySelector("container");
-    
+    const image = document.querySelector(".image");
+    image.alt = json.name;
 
+    // set the img.src property depending on what properties are available in the JSON
+    if (json.image) {
+        image.src = json.image;
+    } else if (json.image_background) {
+        image.src = json.image_background;
+    } else {
+        image.src = "https://media.rawg.io/media/games/328/3283617cb7d75d67257fc58339188742.jpg";
+    }
 
+    const name = document.querySelector(".image");
+    name.innerHTML = json.image_background;
+
+    const gameCount = document.querySelector(".description");
+    gameCount.innerHTML = json.description;
+
+    const description = document.querySelector(".h1");
+    description.innerHTML = json.name;
 }
+
+
+
+
+
+
+
+\   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+
+
 
 
 
 
 // Use either the background_image or background_image_additional property 
 // as the background image of the <div class="image"> element.
+
+/*
+
+function createCreator(json) {
+    console.dir(json);
+
+    const image = document.querySelector(".creator-image");
+    image.alt = json.name;
+
+    // set the img.src property depending on what properties are available in the JSON
+    if (json.image) {
+        image.src = json.image;
+    } else if (json.image_background) {
+        image.src = json.image_background;
+    } else {
+        image.src = "https://via.placeholder.com/250";
+    }
+
+    const name = document.querySelector(".creator-name");
+    name.innerHTML = json.name;
+
+    const gameCount = document.querySelector(".game-count");
+    gameCount.innerHTML = json.games_count;
+
+    const description = document.querySelector(".creator-description");
+    description.innerHTML = json.description;
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
